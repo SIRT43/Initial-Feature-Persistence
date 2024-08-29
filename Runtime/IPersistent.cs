2678af8c1ac5ef8e314c5b42da6ec01471e47679
UT_SIRT43 <utsirt43@163.com>
2024-08-29 23:56:36 +0800

    更改包名

--- Runtime/IPersistent.cs
+++ Runtime/IPersistent.cs
@@ -1,16 +1,18 @@
+using FTGAMEStudio.InitialFramework.IO;
+
 namespace FTGAMEStudio.InitialSolution.Persistence
 {
-    public interface IPersistentReader
+    public interface IPersistentReader : IFileReference
     {
         public bool Read();
     }
 
-    public interface IPersistentWriter
+    public interface IPersistentWriter : IFileReference
     {
         public void Write();
     }
 
-    public interface IPersistent : IPersistentReader, IPersistentWriter
+    public interface IPersistent : IPersistentReader, IPersistentWriter, IFileReference
     {
         public bool Delete();
     }
